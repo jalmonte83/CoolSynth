e76f451cef8da9ddfac27a63bc2225f7ae866b72
Jeffrey Almonte <jalmonte83@gmail.com>
2019-03-08 14:16:56 -0500

    OctaveSwitch able to add 2 Octaves and play notes.  still need to get it to revert to 1 Octave

---
+++ CoolSynth/CoolKeyboardTwoOctView.swift
@@ -0,0 +1,21 @@
+//
+//  CoolKeyboardTwoOctView.swift
+//  CoolSynth
+//
+//  Created by Jeffrey Almonte on 3/8/19.
+//  Copyright © 2019 Jeffrey Almonte. All rights reserved.
+//
+
+import UIKit
+
+class CoolKeyboardTwoOctView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
